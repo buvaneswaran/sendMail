caaf87f8de53ae79ba787c129b3419ded8b4de30
ResulticksInc <int.pbuvaneswaran@gmail.com>
2018-10-12 23:35:48 +0530

    Update mail.js

--- mail.js
+++ mail.js
@@ -16,30 +16,6 @@ app.get('/', function (req, res) {
   res.send('Hello World');
 });
 
-var edmContent = fs.readFileSync("mailedm.html","utf-8");
-
-var transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: "int.rsivakumar@gmail.com",
-    pass: 'siva1234'
-  }
-});
-
-var mailOptions = {
-  from: "int.rsivakumar@gmail.com",
-  to: ["int.rsivakumar@gmail.com", "pbuvaneswaran@interaktmail.com"],
-  subject: 'Sending Email using Node.js',
-  html: edmContent
-};
-
-transporter.sendMail(mailOptions, function(error, info){
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Email sent: ' + info.response);
-  }
-});
 
 
 /* Converting html file into string */
