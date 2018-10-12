26e32612eecacf6680d01f823b7bf94ba0eb3a79
ResulticksInc <int.pbuvaneswaran@gmail.com>
2018-10-12 23:31:45 +0530

    Update mail.js

--- mail.js
+++ mail.js
@@ -12,33 +12,34 @@ app.use(bodyParser.json())
 app.get('/', function (req, res) {
   console.log("welcome");
 
-  var edmContent = fs.readFileSync("mailedm.html","utf-8");
-
-  var transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: "int.rsivakumar@gmail.com",
-      pass: 'siva1234'
-    }
-  });
-  
-  var mailOptions = {
-    from: "int.rsivakumar@gmail.com",
-    to: ["int.rsivakumar@gmail.com", "pbuvaneswaran@interaktmail.com"],
-    subject: 'Sending Email using Node.js',
-    html: edmContent
-  };
-  
-  transporter.sendMail(mailOptions, function(error, info){
-    if (error) {
-      console.log(error);
-    } else {
-      console.log('Email sent: ' + info.response);
-    }
-  });
+  console.log(req.url);
+  res.send('Hello World');
 });
 
+var edmContent = fs.readFileSync("mailedm.html","utf-8");
 
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: "int.rsivakumar@gmail.com",
+    pass: 'siva1234'
+  }
+});
+
+var mailOptions = {
+  from: "int.rsivakumar@gmail.com",
+  to: ["int.rsivakumar@gmail.com", "pbuvaneswaran@interaktmail.com"],
+  subject: 'Sending Email using Node.js',
+  html: edmContent
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
 
 
 /* Converting html file into string */
