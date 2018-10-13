d62bb33de4a5071ceb150cc8f753fcf37c4817e6
ResulticksInc <int.pbuvaneswaran@gmail.com>
2018-10-13 09:42:42 +0530

    Update mail.js

--- mail.js
+++ mail.js
@@ -1,31 +1,25 @@
 
-/* Importing modules */
-var nodemailer = require('nodemailer');
-var fs = require('file-system');
-var edmContent = fs.readFileSync("mailedm.html","utf-8");
+"use strict";
 
-var transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: "int.rsivakumar@gmail.com",
-    pass: 'siva1234'
-  }
+const express = require("express");
+const bodyParser = require("body-parser");
+const restService = express();
+
+
+restService.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+restService.use(bodyParser.json());
+
+
+
+restService.get("/home", function (req, res) {
+  return res.send("200");
 });
 
-var mailOptions = {
-  from: "int.rsivakumar@gmail.com",
-  to: ["int.rsivakumar@gmail.com", "pbuvaneswaran@interaktmail.com","buvanesh.special@gmail.com"],
-  subject: 'Vision Market',
-  html: edmContent
-};
-
-transporter.sendMail(mailOptions, function(error, info){
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Email sent: ' + info.response);
-  }
-});
 
 
 /* Converting html file into string */
